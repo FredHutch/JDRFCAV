d3600e7e31b2dc6e6aa2d3e36c44572725d2eda8
drorberel <dror.berel@gmail.com>
2019-01-25 14:28:55 -0800

    heatmaps

---
+++ R/Dataset_data_task.R
@@ -0,0 +1,14 @@
+#' Dataset, Multi assay data in mlr's task format
+#'
+#' mlr's task (NO functionals implemented YET), therefore, use sep to at analyte name to exatract assay annotation
+#'
+#' @format mlr's task
+#' \describe{
+#'   \item{data_task}{task}
+#'   ...
+#' }
+#' @source JDRF multi center efforts
+"data_task"
+
+
+
