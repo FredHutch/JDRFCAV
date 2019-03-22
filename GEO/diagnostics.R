0779d5494cfb8f4f233a712397703009b9204388
drorberel <dror.berel@gmail.com>
2019-03-22 14:32:00 -0700

    Box files copied to package folder

--- GEO/diagnostics.R
+++ GEO/diagnostics.R
@@ -1,5 +1,5 @@
 ###### GEO
-GEO_mRNA_assay<-read.csv('GEO/CAV_IIandIII_All126wNormPla_PartekRMA_rawLog2Int_2016Dec09_forBRI.csv')
+GEO_mRNA_assay<-read.csv('GEO/mRNA/CAV_IIandIII_All126wNormPla_PartekRMA_rawLog2Int_2016Dec09_forBRI.csv')
 GEO_mRNA_assay %>% dim
 # GEO_mRNA_assay %<>% dplyr::rename(CAIII_c06_911285.H02 = CAIII_c06_911285H02)
 GEO_sample_names<-GEO_mRNA_assay %>% names %>% str_sub(-10, -1)
@@ -37,4 +37,31 @@ mRNA.data_filt[1:3, 1:4]
 
 
 setdiff(mRNA.data_filt$X, mRNA_assay_GEO$X)
+
+
+
+
+
+
+
+##### Compare probe id overlap
+
+mRNA.data_filt.file.box<-"CAV Data Sharing/Affy arrays from serum exposure expt - Hessner/Cohort 2/DataForShinyApp/filteredAffymetrixData_RemovedDateSent.csv"
+mRNA.data_filt<-read.csv(paste(Active.Box.folder, mRNA.data_filt.file.box, sep=''))
+dim(mRNA.data_filt)
+probes_Box<-mRNA.data_filt$X %>% as.character # 21,168
+
+### GEO:
+affyEset # [54,567, 53]
+affyEsetFilter<-nsFilter(affyEset, remove.dupEntrez=FALSE, var.func=IQR, var.cutoff=0.5)$eset
+dim(affyEsetFilter)	# now have 21,168 genes to look at
+
+
+probes_GEO<-featureNames(affyEsetFilter)
+
+setdiff(probes_GEO, probes_Box)
+setdiff(probes_Box, probes_GEO)
+
+list(probes_GEO = probes_GEO, probes_Box = probes_Box) %>% gplots::venn()
+
 
